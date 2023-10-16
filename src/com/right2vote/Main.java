f68c4eed2f757459eefd2f9a977c6f9e616845ad
Ritik rana <99540875+ritikrana0169@users.noreply.github.com>
2023-10-16 17:59:50 +0530

    Update Main.java

--- src/com/right2vote/Main.java
+++ src/com/right2vote/Main.java
@@ -8,25 +8,26 @@ public class Main {
 	public static void main(String[] args) {
 	
 		
-		double amount = 600.00;
+	double budget = 600.00;
         double priceOfTShirt = 50.00;
         double priceOfJeans = 80.00;
         double priceOfJacket = 100.00;
         
 
-        combinations(amount, priceOfTShirt, priceOfJeans, priceOfJacket);
+        combinations(budget, priceOfTShirt, priceOfJeans, priceOfJacket);
 	}
 
-	public static void combinations(double amount, double priceOfTShirt, double priceOfJeans, double priceOfJacket) {
-        int items=0; //TAKING THE COUNT VARIABLE FOR JUST COUNTING COMBINATIONS IN PRINT STATEMENT
-		for (int tShirts=1; tShirts*priceOfTShirt<=amount; ++tShirts) {
-			double remainingAmountAfterTShirt=amount-(tShirts*priceOfTShirt);
-            for (int jeans=1; jeans*priceOfJeans<=remainingAmountAfterTShirt; ++jeans) {
-                int jackets=(int) ((amount-(tShirts*priceOfTShirt)-(jeans*priceOfJeans))/priceOfJacket);
+	public static void combinations(double budget, double priceOfTShirt, double priceOfJeans, double priceOfJacket) {
+        int items=0; 
+		
+		for (int tShirts=1; tShirts*priceOfTShirt<=budget; ++tShirts) {
+	        double remainingAmount=budget-(tShirts*priceOfTShirt);
+            for (int jeans=1; jeans*priceOfJeans<=remainingAmount; ++jeans) {
+                int jackets=(int) ((budget-(tShirts*priceOfTShirt)-(jeans*priceOfJeans))/priceOfJacket);
                 double totalCost=(tShirts*priceOfTShirt)+(jeans*priceOfJeans)+(jackets*priceOfJacket);
-                if (totalCost<=amount  && jackets>=1 && jeans>=1 && tShirts>=1) {
+                if (totalCost<=budget  && jackets>=1 && jeans>=1 && tShirts>=1) {
                 	++items;
-                	System.out.println(items+" ->  "+tShirts+" T-shirts, "+jeans+" Jeans, "+jackets+" Jackets");
+                	System.out.println(items+" --  "+tShirts+" T Shirts, "+jeans+" Jeans, "+jackets+" Jackets");
                 }
             }
         }
